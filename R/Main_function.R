cfe0749e0df6d040cc129ab72cc7146d88ebf6ad
KaiHsiangHu <river19951224@gmail.com>
2020-03-20 13:06:48 +0800

    Show individual figures of each steps

--- R/Main_function.R
+++ R/Main_function.R
@@ -131,11 +131,16 @@ iNEXT.4steps <- function(data, datatype="abundance", size=NULL, endpoint=NULL,
                          cover.RE.plot, even.plot
   )
   if (details==FALSE) {
-    ans <- list(summary = summary, figure = steps.plot)
+    ans <- list(summary = summary,
+                figure = list(SC.plot, size.RE.plot, asy.plot,
+                              cover.RE.plot, even.plot, steps.plot))
   } else {
     tab = list("Sample Completeness" = SC.table, "iNEXT" = RE.table,
                "Asymptotic Diversity" = asy.table, "Evenness" = even.table)
-    ans <- list(summary = summary, figure = steps.plot, details = tab)
+    ans <- list(summary = summary,
+                figure = list(SC.plot, size.RE.plot, asy.plot,
+                              cover.RE.plot, even.plot, steps.plot),
+                details = tab)
   }
   return(ans)
 }
